4f0abac41736989e3410f8628256444d6bd659cd
Luka Kavtaradze <Lk@gmail.com>
2023-12-08 18:16:56 +0400

    arrow styling pt 2

--- script.js
+++ script.js
@@ -4,44 +4,74 @@
 
 
 
-var scroll = document.getElementsByClassName("scroll")
 
-var QA = [...document.getElementsByClassName("QA")];
+
+var QA = document.getElementsByClassName("QA")
+const questionsArray = [...QA]
 
 
 var arrows = document.getElementsByClassName("arrows")
-// console.log(arrows);
+// // console.log(arrows);
+
+let openQuestion = null
 
 
+questionsArray.forEach((question) => {
+  question.addEventListener("click", function () {
 
-QA.forEach((item) => {
-  item.addEventListener("click", function () {
-
-
-
-
-    
-
-    // Find the parent .questions element
     var parentQuestions = this.closest('.questions');
 
-    // Find the .scroll and .answers elements within the parent .questions
+    // // Find the .scroll and .answers elements within the parent .questions
     var currentArrows = parentQuestions.lastElementChild;
-    var currentAnswers = parentQuestions.querySelector('.answers');
+    // var currentAnswers = parentQuestions.querySelector('.answers');
 
-    // Rotate the arrows
+    // // Rotate the arrows
     const degree = 180;
+
+
+
     
-    if(currentAnswers.style.display ==="block") {
-      currentArrows.style.transform = 'rotate(0deg)';
+
+ if(question == openQuestion) {
+  question.nextElementSibling.style.display = "none"
+  openQuestion = null
+ }
+ else{
+  if(openQuestion != null) {
+    
+
+
+    openQuestion.nextElementSibling.style.display  =  "none"
+    
+    
+
+  }
+ 
+  question.nextElementSibling.style.display = "block"
+  openQuestion = question
+ }
+
+ 
+
+
+
+    
+
+
+  
+    
+
+    if(question.nextElementSibling.style.display ==="block") {
+
+
+      currentArrows.style.transform = 'rotate(180deg)';
 
       
 
 
     }
-    if(currentAnswers.style.display === "none") {
-      currentArrows.style.transform = `rotate(${degree}deg)`;
-      currentAnswers.style.font_weight = 18
+    if(question.nextElementSibling.style.display === "none") {
+      currentArrows.style.transform = `rotate(0deg)`;
 
     }
 
@@ -49,16 +79,12 @@ QA.forEach((item) => {
     
 
 
-    // Toggle the display property of the answers element
-
-    currentAnswers.style.display = (currentAnswers.style.display === "none") ? "block" : "none";
+    
   });
 
 
 })
  
-
-
 
 
 
